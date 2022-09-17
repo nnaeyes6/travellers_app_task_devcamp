8da385d60a91e6b76b0175c5366f58381916063c
Anya Mbah <nnaeyesworld@gmail.com>
2022-09-17 21:28:12 +0100

    Task completed

---
+++ lib/model/data_model.dart
@@ -0,0 +1,63 @@
+class DatatModel {
+  late String cityName;
+  late String cityImage;
+  late String cityInfo;
+  DatatModel({
+    required this.cityName,
+    required this.cityImage,
+    required this.cityInfo,
+  });
+}
+
+List<DatatModel> datalist = [
+  DatatModel(
+      cityName: "London",
+      cityImage: "images/london.jpg",
+      cityInfo:
+          "London is the capital and largest city of England and the United Kingdom, with a population of just over 9 million."),
+  DatatModel(
+      cityName: "Paris",
+      cityImage: "images/paris.jpg",
+      cityInfo:
+          "Paris is the capital and most populous city of France, with an estimated population of 2,165,423 residents in 2019 in an area of more than 105 km²."),
+  DatatModel(
+      cityName: "NewYork",
+      cityImage: "images/newyork.jpg",
+      cityInfo:
+          "New York, often called New York City (NYC) to distinguish it from the State of New York, is the most populous city in the United States."),
+  DatatModel(
+      cityName: "Singapore",
+      cityImage: "images/singapore.jpg",
+      cityInfo:
+          "Singapore, city-state located at the southern tip of the Malay Peninsula, about 85 miles (137 kilometres) north of the Equator."),
+  DatatModel(
+      cityName: "Berlin",
+      cityImage: "images/berlin.jpg",
+      cityInfo:
+          "Singapore, city-state located at the southern tip of the Malay Peninsula, about 85 miles (137 kilometres) north of the Equator."),
+  DatatModel(
+      cityName: "Cairo",
+      cityImage: "images/cairo1.jpg",
+      cityInfo:
+          "Singapore, city-state located at the southern tip of the Malay Peninsula, about 85 miles (137 kilometres) north of the Equator."),
+  DatatModel(
+      cityName: "Lagos",
+      cityImage: "images/lagos.jpg",
+      cityInfo:
+          "Singapore, city-state located at the southern tip of the Malay Peninsula, about 85 miles (137 kilometres) north of the Equator."),
+  DatatModel(
+      cityName: "Tokyo",
+      cityImage: "images/tokyo.jpg",
+      cityInfo:
+          "Singapore, city-state located at the southern tip of the Malay Peninsula, about 85 miles (137 kilometres) north of the Equator."),
+  DatatModel(
+      cityName: "Madrid",
+      cityImage: "images/madrid.jpg",
+      cityInfo:
+          "Singapore, city-state located at the southern tip of the Malay Peninsula, about 85 miles (137 kilometres) north of the Equator."),
+  DatatModel(
+      cityName: "Delhi",
+      cityImage: "images/delhi.jpg",
+      cityInfo:
+          "Singapore, city-state located at the southern tip of the Malay Peninsula, about 85 miles (137 kilometres) north of the Equator."),
+];
